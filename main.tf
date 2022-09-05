908050b987a38fcb8c6be0942689c08803127564
jair.junior3 <jair.junior@mutual.club>
2022-09-05 14:51:57 -0300

    second commit

--- main.tf
+++ main.tf
@@ -6,17 +6,3 @@ resource "aws_ssoadmin_permission_set" "permset" {
   session_duration = "PT2H"
 }
 
-
-
-resource "aws_ssoadmin_managed_policy_attachment" "managepolicy" {
-  instance_arn       = tolist(data.aws_ssoadmin_instances.datainstance.arns)[0]
-  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  permission_set_arn = aws_ssoadmin_permission_set.permset.arn
-}
-
-resource "aws_ssoadmin_managed_policy_attachment" "managepolicy1" {
-  instance_arn       = tolist(data.aws_ssoadmin_instances.datainstance.arns)[0]
-  managed_policy_arn = "arn:aws:iam::aws:policy/AWSDirectConnectReadOnlyAccess"
-  permission_set_arn = aws_ssoadmin_permission_set.permset.arn
-}
-
--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "descrição" {
   type    = string
   default = "infra"
 }
-
-########### VAR BIGDATE ###########
-
-#variable "namebig" {
- # type    = string
- # default = "BigDate"
-#}
-
-#variable "descriçãobig" {
- # type    = string
- # default = "dados"
-#}
